2d1e57ea56a96f8e8d36d669e2bdfdd80edc0eb5
Andrew Kappen <akappen@gmail.com>
2013-01-30 14:23:11 -0600

    Emacs 24 based configuration.

---
+++ README.md
@@ -0,0 +1,13 @@
+Emacs 24 Configuration
+======================
+
+1.  Clone the repo
+    `git clone git://github.com/akappen/emacs-configuration.git`
+
+2.  Symlink ~/.emacs.d to the repo
+    `ln -s ~/.emacs.d emacs-configuration`
+
+3.  Install the packages
+	`emacs --funcall my-packages-install`
+
+4.  Use it
---
+++ init.el
@@ -0,0 +1,94 @@
+;;; required package configuration
+
+;; melpa package archive
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+;; install or update packages
+(defvar my-packages
+  (list 'magit
+	'browse-kill-ring
+	'linum-off
+	'coffee-mode
+	'haml-mode
+	'rspec-mode
+	'ruby-tools
+	'flymake-ruby
+	'rinari
+	'markdown-mode
+	'twilight-theme))
+(defun my-packages-install ()
+  "Install my packages."
+  (interactive)
+  (dolist (package my-packages)
+    (unless (or (member package package-activated-list)
+                (functionp package))
+      (message "Installing %s" (symbol-name package))
+      (package-install package))))
+
+
+;;; global configuration
+
+;; interface tweaks
+(setq inhibit-splash-screen t)    ; no welcome screen
+(tool-bar-mode 0)                 ; no icon bar
+(scroll-bar-mode 0)               ; no scrollbar
+(global-linum-mode t)             ; line numbers
+(require 'linum-off)              ;   where appropriate
+(column-number-mode t)            ; modeline column numbers
+(winner-mode t)                   ; window layout history
+(defalias 'yes-or-no-p 'y-or-n-p) ; short prompts
+(setq make-backup-files nil)      ; no ~ backup files
+(load-theme 'twilight t)          ; tango-dark is nice too
+(add-hook 'before-save-hook       ; clean whitespace on save
+	  'delete-trailing-whitespace)
+(electric-pair-mode t)            ; pair quotes and braces
+
+;; remap other-window to M-o
+(global-set-key (kbd "M-o") 'other-window)
+
+;; C-h and C-M-h as delete and delete word
+(global-set-key (kbd "C-h") 'backward-delete-char-untabify)
+(global-set-key (kbd "C-M-h") 'backward-kill-word)
+
+;; grep-find ignores
+(grep-compute-defaults)
+(add-to-list 'grep-find-ignored-directories "log")
+(add-to-list 'grep-find-ignored-directories "tmp")
+
+;;; package specific configuration
+
+;; magit
+(setq magit-status-buffer-switch-function 'switch-to-buffer) ; status opens full-frame
+(global-set-key "\C-xg" 'magit-status)
+
+;; browse-kill-ring
+(browse-kill-ring-default-keybindings) ; M-y opens browser
+
+;; coffee-mode
+(require 'coffee-mode)
+(setq coffee-tab-width 2)
+
+;; ruby-tools
+(require 'ruby-tools) ; needed to hook ruby-mode
+
+;; flymake-ruby
+(require 'flymake-ruby)
+(add-hook 'ruby-mode-hook 'flymake-ruby-load)
+
+;; rinari
+(require 'rinari)
+(global-rinari-mode)
+
+;; ido
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+;; org-mode
+(setq org-directory "~/org")
+(setq org-agenda-files (directory-files org-directory 1 "\.org$"))
+(setq org-agenda-clockreport-parameter-plist (quote (:fileskip0 t)))
+(global-set-key "\C-ca" 'org-agenda)
